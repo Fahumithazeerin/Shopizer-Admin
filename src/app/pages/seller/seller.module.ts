51ff6cae9487e7ce8941f5fc434b263f1a941302
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-11-15 15:03:29 +0530

    merged conflicts and corrected front end

--- src/app/pages/seller/seller.module.ts
+++ src/app/pages/seller/seller.module.ts
@@ -11,12 +11,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { SellerListComponent } from './seller-list/seller-list.component';
 import { NbDialogModule } from '@nebular/theme';
 import { SellerDetailEditComponent } from './seller-detail-edit/seller-detail-edit.component';
-import { ProductsComponent } from './seller-products/products.component';
+import { ProductsComponent } from './seller-products/seller-products.component';
 import { ProductsListComponent } from './seller-products/products-list/products-list.component';
 import { ProductCreationComponent } from './seller-products/product-creation/product-creation.component';
 import { ProductOrderListComponent } from './order-management/order-management.component';
 import { CustomModule } from '../custom-component/custom.module';
-
+import { ProductFormComponent } from './seller-products/product-form/product-form.component';
 import { NgxSummernoteModule } from 'ngx-summernote';
 import { AvailableButtonComponent } from './seller-products/products-list/available-button.component';
 
